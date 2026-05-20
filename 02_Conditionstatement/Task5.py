249941459983c6d1d217c99d74e807d6bc96db65
Hariom <Hariomharish50@gmail.com>
2026-05-20 23:37:33 +0530

    new updatd file

--- 02_Conditionstatement/Task5.py
+++ 02_Conditionstatement/Task5.py
@@ -7,3 +7,14 @@
 # ○ Tax Rate = 10%
 # ○ Other Charges = ₹2000
 # Display the Gross Salary Tax and Net Salary.
+
+Base_salary= 50000
+Bonus=5000
+Tax_Rate=10
+other_charge=2000
+Gross_salary=Base_salary+Bonus
+Tax=10/100*Gross_salary
+net_salary=Gross_salary-Tax-other_charge
+print(f"Your Gross Salary ",Gross_salary)
+print(f"Your Net Salary afte Tax and other charges" ,net_salary)
+print(f"Your Tax ",Tax)
