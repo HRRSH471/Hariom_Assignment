bcafcb96044047d10e3cbae8c79d2740ba2b1f0c
Hariom <Hariomharish50@gmail.com>
2026-05-22 00:03:34 +0530

    new file updated

--- 02_Conditionstatement/Task15.py
+++ 02_Conditionstatement/Task15.py
@@ -6,3 +6,19 @@
 # 4. Mercedes: 30% tax for prices between 20L and 25L.
 # 5. Input: The car brand and price.
 # 6. Output: The calculated tax on the purchase.
+car_brand=input("Enter the car brand :")
+car_price=float(input("Enter the car price :")) 
+if car_brand == "Mahindra" and car_price >= 700000 and car_price <= 1000000:
+    tax = car_price * 0.05
+    print(f"The tax on the purchase is: Rs. {tax:.2f}")
+elif car_brand == "Audi" and car_price >= 1000000 and car_price <= 1500000:
+    tax = car_price * 0.10
+    print(f"The tax on the purchase is: Rs. {tax:.2f}")
+elif car_brand == "Jaguar" and car_price >= 1500000 and car_price <= 2000000:
+    tax = car_price * 0.25
+    print(f"The tax on the purchase is: Rs. {tax:.2f}")
+elif car_brand == "Mercedes" and car_price >= 2000000 and car_price <= 2500000:
+    tax = car_price * 0.30
+    print(f"The tax on the purchase is: Rs. {tax:.2f}")
+else:
+    print("Invalid car brand or price.")
