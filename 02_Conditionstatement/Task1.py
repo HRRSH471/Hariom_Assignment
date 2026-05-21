dcd8485e8d94c645c21a72f4e22b517038b4ab2b
Hariom <Hariomharish50@gmail.com>
2026-05-21 18:16:27 +0530

    new updated

--- 02_Conditionstatement/Task1.py
+++ 02_Conditionstatement/Task1.py
@@ -1,13 +1,13 @@
 # Task Calculate the Profit Percentage
 cost_price = float(input("Enter the cost price of the item :"))
-selling_prince= float(input("Enter the selling price of the item :"))
+selling_price= float(input("Enter the selling price of the item :"))
 
-if selling_prince > cost_price:
-    profit = selling_prince - cost_price
+if selling_price > cost_price:
+    profit = selling_price - cost_price
     profit_percentage = (profit / cost_price) * 100
     print(f"The profit percentage is: {profit_percentage:.2f}%")
-elif selling_prince < cost_price:
-    loss = cost_price - selling_prince
+elif selling_price < cost_price:
+    loss = cost_price - selling_price
     loss_percentage = (loss / cost_price) * 100
     print(f"The loss percentage is: {loss_percentage:.2f}%")
 else:
--- 02_Conditionstatement/Task10.py
+++ 02_Conditionstatement/Task10.py
@@ -15,3 +15,18 @@
 # ● Marks: 85 → Grade: B
 # ● Marks: 45 → Grade: F
 # ● Marks: 105 → Invalid marks
+marks=int(input("Enter your marks :"))
+if marks >= 90 and marks <=100:
+    print("Your Grade is A")
+elif marks >= 80 and marks <=89:
+    print("Your Grade is B")  
+elif marks >= 70 and marks <=79:
+    print("Your Grade is C") 
+elif marks >=60 and marks <= 69:
+    print("Your Grade is D")   
+elif marks >=50 and marks <=59:
+    print("Your Grade is E")
+elif marks >=0 and marks <=49:
+    print("Your Grade is F") 
+else:
+    print("Invalid Marks Enter")        
