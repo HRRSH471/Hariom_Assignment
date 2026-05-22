a222e1a88ac571db5d3a1e2ea4cfc0605c3197aa
Hariom <Hariomharish50@gmail.com>
2026-05-22 18:34:22 +0530

    new file updatd

--- 02_Conditionstatement/Task17.py
+++ 02_Conditionstatement/Task17.py
@@ -2,3 +2,12 @@
 # ○ Task: Write a program to find greatest number from three number
 # ○ Input: Prompt the user to enter three numbers.
 # ○ Output: Display the greatest number.
+num1=int(input("Enter the First Number : "))
+num2=int(input("Enter the Second Number : "))
+num3=int(input("Enter the Third Number : "))
+if num1 >= num2 and num1 >= num3:
+    print(f"Greatest Number is : ",num1)
+elif num2 >=num1 and num2 >= num3:
+    print(f"Greatest Number is : " , num2)
+elif num3 >= num1 and num3 >= num2:
+    print(f"Greatest Number is : ",num3)
