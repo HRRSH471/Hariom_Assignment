a83382a1d722c071fdae7d9ef118b55164801f42
Hariom <Hariomharish50@gmail.com>
2026-05-22 19:45:20 +0530

    new file updated

--- 02_Conditionstatement/Task20.py
+++ 02_Conditionstatement/Task20.py
@@ -7,3 +7,23 @@
 # ■ 11 to 15 days: ₹4/day.
 # ■ More than 15 days: ₹5/day.
 # ○ Output: Display the total charges
+Name=input("Enter Your Name : ")
+day=int(input("Please enter the days for your borrow the book : "))
+if day<=5:
+    library_charge=day*2
+    print(Name,"Your Total Library Charge is : ",library_charge)
+elif day>=6 and day<=10:
+    library_charge=day*3
+    print(Name,"Your Total Library Charge is : ",library_charge)
+elif day>=11 and day<=15:    
+    library_charge=day*4
+    print(Name,"Your Total Library Charge is : ",library_charge)
+elif day>15:
+    library_charge=day*5
+    print(Name,"Your Total Library Charge is : ",library_charge)
+
+
+
+
+
+ 
