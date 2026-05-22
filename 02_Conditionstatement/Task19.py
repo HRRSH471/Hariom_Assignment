521f7f89e03986fc1b5014cfefe0a5909e1a6d20
Hariom <Hariomharish50@gmail.com>
2026-05-22 19:10:42 +0530

    new file updated

--- 02_Conditionstatement/Task19.py
+++ 02_Conditionstatement/Task19.py
@@ -5,3 +5,13 @@
 # ■ Attendance percentage < 75%: Not eligible to sit in the exam.
 # ■ Attendance percentage ≥ 75%: Eligible to sit in the exam.
 # ○ Output: Display the attendance percentage and eligibility status.
+
+# 19.Calculate Class Attendance Percentage
+Name=input("Enter Your Name : ")
+clas_addenence=int(input("Enter the No of Days You Attend the Class in 365 Days: "))
+attendence_percentage=clas_addenence/365*100
+
+if attendence_percentage >=75:
+    print(Name,"You are eligible for exam because your attendence percentage is ",attendence_percentage)
+else:
+    print(Name,"You are not eligible for exam because your attendence percentage is " ,attendence_percentage)
