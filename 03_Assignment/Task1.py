84379a25fc62740599a6a488a429de17ccbc77b1
Hariom <Hariomharish50@gmail.com>
2026-05-26 21:08:46 +0530

    new file updated

---
+++ 03_Assignment/Task1.py
@@ -0,0 +1,6 @@
+for i in range(1,21):
+    if i%2==0:
+        print(f"The number : {i} is an Even number")
+    else:
+        print(f"The number : {i} is an Odd Number")
+        print("**************************************")
