8e8f31d63aa8a6b728d8b68f7f981441c30edc08
Hariom <Hariomharish50@gmail.com>
2026-05-26 22:19:50 +0530

    new file updatd

--- 03_Assignment/Task5.py
+++ 03_Assignment/Task5.py
@@ -1,2 +1,6 @@
-
-
+start_point=int(input("Enter the starting number : "))
+End_point=int(input("Enter the starting number : "))
+sum=0
+for i in range(start_point+1,End_point):
+    sum+=i
+print(sum)
